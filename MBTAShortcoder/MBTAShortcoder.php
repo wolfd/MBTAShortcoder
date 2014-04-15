e0770b4f7e61a60e0fb665a78f46a3e39ca1a68d
wolfd <2dawolf@gmail.com>
2014-04-15 16:23:00 -0400

    Changed plugin name and file names

--- MBTAShortcoder/MBTAShortcoder.php
+++ MBTAShortcoder/MBTAShortcoder.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: dds-mbta-status
+ * Plugin Name: MBTAShortcoder
  * Plugin URI: http://crew.ccs.neu.edu
  * Description: Provides a shortcode to display MBTA subway statuses for given stops.
  * Version: 0.1
@@ -8,11 +8,11 @@
  * Author URI: http://crew.ccs.neu.edu
  */
 
-include_once("MBTAShortcoder.php");
+include_once("MBTAShortcoder.class.php");
 
 if (class_exists('MBTAShortcoder')) {
-    register_activation_hook("MBTAShortcoder.php", array("MBTAShortcoder", "activate"));
-    register_deactivation_hook("MBTAShortcoder.php", array("MBTAShortcoder", "deactivate"));
+    register_activation_hook("MBTAShortcoder.class.php", array("MBTAShortcoder", "activate"));
+    register_deactivation_hook("MBTAShortcoder.class.php", array("MBTAShortcoder", "deactivate"));
 
     $dds_mbta_status = new MBTAShortcoder();
 }
